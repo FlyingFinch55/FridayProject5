f50831984c85bbf2d3c6ee3f9ef51fd0bd29b4fc
Kimberlee Finch <kmfinch42@tntech.edu>
2024-03-01 00:45:48 -0600

    Uplading the while loop for the quiz bowl program finsihed?

--- NewQuizBowl.py
+++ NewQuizBowl.py
@@ -20,7 +20,21 @@ for row in rows:
 selected_questions = random.sample(questions, 5)
 
 
-
 #Keep this line for testing becouse I can not spell the answers correctly
 #Also making the assumtion that the user will know how to spell correct answers and the full answers 
-print(selected_questions)
+#print(selected_questions)
+
+
+#A for loop that goes through the 5 random questions
+score = 0
+for TheQuestion, CorrAnswer in selected_questions:
+    print(TheQuestion)
+    userAns = input("Your Answer: ")
+    if userAns.lower() == CorrAnswer.lower():
+        print("Correct!")
+        score = score + 1
+    if userAns.lower() != CorrAnswer.lower():
+        print("Wrong. The correct answer is " + CorrAnswer)
+
+print ("Quiz End. Your score is " + str(score) + "/5")
+
